5b40ffc5056da95d78b882597f44582e8373717d
Yuudai Tamura <y.tamu63@gmail.com>
2024-11-18 11:52:17 +0900

    reactの導入

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  server: {
+    proxy: {
+      "/.netlify/functions": "http://localhost:9999",
+    },
+  },
+  plugins: [react()],
+});
